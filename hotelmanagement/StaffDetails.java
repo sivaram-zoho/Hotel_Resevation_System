2b3eb17726c1c73ac915bb33f91dde3932079620
sivaram-zoho <sivaram.a@zohointern.com>
2025-09-10 11:39:02 +0530

    Update staff id as random generated

--- hotelmanagement/StaffDetails.java
+++ hotelmanagement/StaffDetails.java
@@ -1,7 +1,6 @@
 package HotelReservationSystem.hotelmanagement;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class StaffDetails {
     int staffID;
@@ -10,31 +9,39 @@ public class StaffDetails {
     public VariousRole staffRole;
 
     public StaffDetails(int staffID, String staffName, long staffPhone, VariousRole staffRole) {
-        this.staffID = staffID;
+        this.staffID = generateStaffID();
         this.staffName = staffName;
         this.staffPhone = staffPhone;
         this.staffRole = staffRole;
     }
 
+    public static int generateStaffID() {
+        Random random = new Random();
+        return random.nextInt(2000, 3000);
+    }
+
     public static List<StaffDetails> staffList = new ArrayList<>();
 
     static {
-        staffList.add(new StaffDetails(1, "Shiva", 7373737420L, StaffDetails.VariousRole.MANAGER));
-        staffList.add(new StaffDetails(2, "Raja", 9883949583L, StaffDetails.VariousRole.RECEPTIONIST));
-        staffList.add(new StaffDetails(3, "Sai", 9475949396L, StaffDetails.VariousRole.MANAGER));
-        staffList.add(new StaffDetails(4, "Balaji", 9274760385L, StaffDetails.VariousRole.RECEPTIONIST));
-        staffList.add(new StaffDetails(5, "Adhi", 9284534789L, StaffDetails.VariousRole.HOUSE_KEEPING));
-        staffList.add(new StaffDetails(6, "Murali", 9023676354L, StaffDetails.VariousRole.RECEPTIONIST));
-        staffList.add(new StaffDetails(7, "Ruban", 7584739483L, StaffDetails.VariousRole.HOUSE_KEEPING));
-        staffList.add(new StaffDetails(8, "Jimmy", 8937462980L, StaffDetails.VariousRole.RECEPTIONIST));
-        staffList.add(new StaffDetails(9, "Karthik", 98327465901L, StaffDetails.VariousRole.HOUSE_KEEPING));
-        staffList.add(new StaffDetails(10, "Guna", 8870090439L, StaffDetails.VariousRole.HOUSE_KEEPING));
+        staffList.add(new StaffDetails(0, "Shiva", 7373737420L, StaffDetails.VariousRole.MANAGER));
+        staffList.add(new StaffDetails(0, "Sai", 9475949396L, StaffDetails.VariousRole.MANAGER));
+        staffList.add(new StaffDetails(0, "Jack", 9938423049L, StaffDetails.VariousRole.MANAGER));
+        staffList.add(new StaffDetails(0, "Joe", 9728301938L, StaffDetails.VariousRole.MANAGER));
+        staffList.add(new StaffDetails(0, "Raja", 9883949583L, StaffDetails.VariousRole.RECEPTIONIST));
+        staffList.add(new StaffDetails(0, "Balaji", 9274760385L, StaffDetails.VariousRole.RECEPTIONIST));
+        staffList.add(new StaffDetails(0, "Adhi", 9284534789L, StaffDetails.VariousRole.HOUSE_KEEPING));
+        staffList.add(new StaffDetails(0, "Murali", 9023676354L, StaffDetails.VariousRole.RECEPTIONIST));
+        staffList.add(new StaffDetails(0, "Ruban", 7584739483L, StaffDetails.VariousRole.HOUSE_KEEPING));
+        staffList.add(new StaffDetails(0, "Jimmy", 8937462980L, StaffDetails.VariousRole.RECEPTIONIST));
+        staffList.add(new StaffDetails(0, "Karthik", 98327465901L, StaffDetails.VariousRole.HOUSE_KEEPING));
+        staffList.add(new StaffDetails(0, "Guna", 8870090439L, StaffDetails.VariousRole.HOUSE_KEEPING));
     }
 
- 
+
     public static List<StaffDetails> getStaffList() {
         return staffList;
     }
+
     public static void setStaffList(List<StaffDetails> staffList) {
         StaffDetails.staffList = staffList;
     }
@@ -68,6 +75,7 @@ public class StaffDetails {
     public int getStaffID() {
         return staffID;
     }
+
 
     public long getStaffPhone() {
         return staffPhone;
